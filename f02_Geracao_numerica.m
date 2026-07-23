b263b75356d3c6d864e94edf7eaf955b647523c5
Gabriel C. Passos <69571479+Gab-cp011@users.noreply.github.com>
2026-07-23 18:18:12 -0300

    Refactor numerical generation script with updated parameters

--- f02_Geracao_numerica.m
+++ f02_Geracao_numerica.m
@@ -5,39 +5,47 @@ clc; clear; close all;
 fprintf('Inicializando a modelagem do sistema dinâmico...\n');
 
 % 1. Parâmetros geométricos e propriedades nominais
-bc = 0.025; 
-hc_normal = 0.006; 
-L = 0.177 - 0.025;
-I_normal = (bc * hc_normal^3) / 12;
+bc = 0.025; % largura da coluna
+hc_normal = 0.006; % espessura da coluna íntegra
+L = 0.152; % comprimento da coluna
+I_normal = (bc * hc_normal^3) / 12; 
 
 % Massas dos componentes (kg)
-mf = 6.26; mc = 0.06; mbumper = 0.20; msp = 0.26; mcf = 0.04;
+mf = 6.26; % massa da placa de alumínio de cada andar
+mc = 0.06; % massa da coluna de alumínio
+mbumper = 0.20; % massa do bumper
+msp = 0.26; % massa da coluna suspensa do mecanismo de dano 
+mcf = 0.04; % massa do bloco de parafuso da coluna
 
 % Matriz de massa nominal (sem adições operacionais)
-m1_nom = mf + mc*4/2 + mcf*4; 
-m2_nom = mf + mc*4 + mcf*4; 
-m3_nom = mf + mc*4 + mcf*4 + msp; 
-m4_nom = mf + mc*4/2 + mcf*4 + mbumper; 
+m1_nom = mf + mc*4/2 + mcf*4; % massa da base
+m2_nom = mf + mc*4 + mcf*4; % massa do primeiro andar
+m3_nom = mf + mc*4 + mcf*4 + mbumper; % massa do segundo andar
+m4_nom = mf + mc*4/2 + mcf*4 + msp; % massa do terceiro andar
 
 % Razões de amortecimento extraídas do modelo experimental (dcv)
 dcv = [0.0001; 0.063; 0.020; 0.0097];
 
 % 2. Mapeamento estocástico do Módulo de Elasticidade
-mu_E = 65e9;
-sigma_E = 0.15e9;
-A_gamma = (mu_E / sigma_E)^2; 
+mu_E = 65e9; % média assumida do material
+sigma_E = 0.15e9; % desvio padrão
+
+% parâmetros da distribuição Gamma
+A_gamma = (mu_E / sigma_E)^2;  
 B_gamma = sigma_E^2 / mu_E;
 
 % Semente fixa para garantir a reprodutibilidade da incerteza
 rng(42);
 amostras_totais = 150 * 9; % 150 iterações para 9 condições
+% sorteia os valores de rigidez do material em cada simulação
 E_samples = gamrnd(A_gamma, B_gamma, [amostras_totais, 1]);
 
 % 3. Configuração da progressão do dano
 hc_array = 0.0045:-0.0003:0.0015;
 N_rodadas = length(hc_array);
 
-% Estruturas de armazenamento para as 11 rodadas
+% Estruturas de armazenamento para as 11 rodadas, representando a redução
+% de espessura gradativa
 Xs_todas = cell(N_rodadas, 1);
 Labels_todas = cell(N_rodadas, 1);
 
@@ -46,9 +54,10 @@ fprintf('Calculando frequências teóricas para %d rodadas de degradação...\n'
 % 4. Loop principal de simulação
 for rodada = 1:N_rodadas
     hc_dano = hc_array(rodada);
-    I_dano = (bc * hc_dano^3) / 12;
+    I_dano = (bc * hc_dano^3) / 12; %  inércia de coluna com dano
     
-    % Fatores de redução de rigidez (proporção da inércia preservada)
+    % Fatores de redução de rigidez (percentual entre a rigidez reduzida e
+    % a rigidez "inicial")
     fator_1col = (3 * I_normal + 1 * I_dano) / (4 * I_normal);
     fator_2col = (2 * I_normal + 2 * I_dano) / (4 * I_normal);
     
@@ -71,19 +80,19 @@ for rodada = 1:N_rodadas
                 
             % Dano Nível 1 (Troca de 1 coluna)
             case 4
-                mult_rigidez(1) = fator_1col; % Dano no 1º andar
+                mult_rigidez(1) = fator_1col; % Dano 1 no 1º andar
             case 5
-                mult_rigidez(2) = fator_1col; % Dano no 2º andar
+                mult_rigidez(2) = fator_1col; % Dano 1 no 2º andar
             case 6
-                mult_rigidez(3) = fator_1col; % Dano no 3º andar
+                mult_rigidez(3) = fator_1col; % Dano 1 no 3º andar
                 
             % Dano Nível 2 (Troca de 2 colunas)
             case 7
-                mult_rigidez(1) = fator_2col; % Severo no 1º andar
+                mult_rigidez(1) = fator_2col; % Dano 2 no 1º andar
             case 8
-                mult_rigidez(2) = fator_2col; % Severo no 2º andar
+                mult_rigidez(2) = fator_2col; % Dano 2 no 2º andar
             case 9
-                mult_rigidez(3) = fator_2col; % Severo no 3º andar
+                mult_rigidez(3) = fator_2col; % Dano 2 no 3º andar
         end
         
         % Consolidação da matriz de massa do cenário atual
@@ -116,10 +125,7 @@ for rodada = 1:N_rodadas
             
             % 3. Matriz de amortecimento modal e reconstrução da matriz C física
             A_modal = diag(2 .* dcv .* wn .* Mn);
-            C = inv(V') * A_modal * inv(V);
-            
-            % Limpeza de resíduos numéricos flutuantes para garantir simetria
-            C = (C + C') / 2;
+            C= (V'\A_modal)/V;
             
             % 4. Montagem da Matriz de Estado global (SSA)
             SSA = [zeros(4), eye(4); -M\K, -M\C];
@@ -131,6 +137,7 @@ for rodada = 1:N_rodadas
             lambda = autovalores(imag(autovalores) > 0);
             
             % A frequência registrada é a parte imaginária do autovalor
+            % (frequências naturais amortecidas)
             freq_amortecida = sort(imag(lambda) / (2*pi));
             
             % D. Seleção dos Modos de Flexão
